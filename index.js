f2e0483348d47d420850b1ffa8ca0c566b460fec
Jayesh <jayeshjaiswal2510@gmail.com>
2024-09-26 22:42:00 +0530

    updatess

--- index.js
+++ index.js
@@ -15,25 +15,16 @@ const dbString = process.env.DB_STRING;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/finance-record", router);
 
-// Routes
-app.get("/", (req, res) => {
-  res.send({ message: "Server is running!!" });
-});
-
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log(`Server running smoothing 4000`);
 });
-
-app.use("/finance-record", router);
 
 // MongoDB Connection (no deprecated options needed)
 mongoose
   .connect(dbString)
   .then(() => {
-    app.listen(port, () => {
-      console.log(`Server running smoothing`);
-    });
     console.log("Connected to the database");
   })
   .catch((error) => {
