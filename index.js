0e4508f3a5ee39d28dfd9915e3b68013f2ab5b5a
Jayesh <jayeshjaiswal2510@gmail.com>
2024-09-26 20:41:22 +0530

    Added Minor changes

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
+import * as dotenv from "dotenv";
+import connectDB from "./MongoDB/connect.js";
+import mongoose from "mongoose";
 // import router from "./routes/FinancialRoute.js";
-import dotenv from "dotenv";
-
-// Initialize Express
-const app = express();
 
 dotenv.config();
 
-const port = process.env.PORT || 3000; // Use PORT instead of VITE_PORT
-const dbString = process.env.DB_STRING; // Use DB_STRING instead of VITE_DB_STRING
-
-// Middleware
+const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
-// Routes
+const startServer = async () => {
+  try {
+    await connectDB(process.env.DB_STRING);
+    const port = process.env.PORT || 8080;
+    app.listen(port, () => console.log(`Server started on port ${port}`));
+  } catch (error) {
+    console.error(`Error starting server: ${error.message}`);
+  }
+};
+
+startServer();
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to the database");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(`Mongoose connection error: ${err}`);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
+});
+
 app.get("/", (req, res) => {
   res.send({ message: "Server is running!!" });
 });
 
 // app.use("/finance-record", router);
 
-// MongoDB Connection
-mongoose
-  .connect(dbString, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-    console.log("Connected to the database");
-  })
-  .catch((error) => {
-    console.error("Database connection failed:", error);
-  });
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Internal Server Error", err.message);
+});
