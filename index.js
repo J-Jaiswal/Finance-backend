900b94f2551ea2a32953b13fa495f31605a1e94c
Jayesh <jayeshjaiswal2510@gmail.com>
2024-09-26 20:34:47 +0530

    Added Minn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import router from "./routes/FinancialRoute.js";
-import "dotenv/config";
+// import router from "./routes/FinancialRoute.js";
+import dotenv from "dotenv";
 
 // Initialize Express
 const app = express();
 
-// Load environment variables (uncomment if needed)
-require("dotenv").config();
+dotenv.config();
 
 const port = process.env.PORT || 3000; // Use PORT instead of VITE_PORT
 const dbString = process.env.DB_STRING; // Use DB_STRING instead of VITE_DB_STRING
@@ -22,7 +21,7 @@ app.get("/", (req, res) => {
   res.send({ message: "Server is running!!" });
 });
 
-app.use("/finance-record", router);
+// app.use("/finance-record", router);
 
 // MongoDB Connection
 mongoose
